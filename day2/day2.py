12f031e69fc595fb3ee3429b352b6ba9830d21d7
fifisaac <99099211+fifisaac@users.noreply.github.com>
2024-12-02 19:29:43 +0000

    day2 pt2 finally

--- day2/day2.py
+++ day2/day2.py
@@ -3,40 +3,57 @@ with open("input") as f:
 
 items = [i.split() for i in lines]
 
+def isSafe(item):
+
+    safe = True
+
+    if int(item[0]) - int(item[1]) > 0:
+        increasing = False
+    else:
+        increasing = True
+
+    for j in range(0, len(item) - 1):
+
+        diff = int(item[j]) - int(item[j+1])
+    
+        if increasing and diff >= 0:
+            safe = False
+        elif not increasing and diff <= 0:
+            safe = False
+
+        if abs(diff) > 3:
+            safe = False
+
+    return safe
+    
+
 def pt1():
 
     total = 0
 
     for i in items:
 
-        safe = True
-
-        if int(i[0]) - int(i[1]) > 0:
-            increasing = False
-        else:
-            increasing = True
-
-        for j in range(0, len(i) - 1):
-
-            diff = int(i[j]) - int(i[j+1])
-        
-            if increasing and diff >= 0:
-                safe = False
-            elif not increasing and diff <= 0:
-                safe = False
-
-            if abs(diff) > 3:
-                safe = False
-
-        if safe:
+        if isSafe(i):
             total += 1
 
     return total
 
 
 def pt2():
-    pass
 
+    total = 0
+
+    for i in items:
+
+        for j in range(0, len(i)):
+
+            missing = i[:j] + i[(j+1):]
+
+            if isSafe(missing):
+                total += 1
+                break
+
+    return total
 
 
 print(f'Part 1: {pt1()}')
