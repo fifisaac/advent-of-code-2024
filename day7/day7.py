0f34423bbe26ec5ff9f276469d156843ef67e84e
fifisaac <99099211+fifisaac@users.noreply.github.com>
2024-12-08 12:54:21 +0000

    updated day 7

--- day7/day7.py
+++ day7/day7.py
@@ -23,65 +23,80 @@ def pt1():
 
     for i, j in enumerate(eqns):
         j = [int(k) for k in j]
-        # s = sum(j)
+        s = sum(j)
 
-        # if s == int(solns[i]):
-        #     total += int(solns[i])
-        #     continue
-        # elif s > int(solns[i]):
-        #     continue
-        # m = product(j)
-        # if m == int(solns[i]):
-        #     total += int(solns[i])
-        #     continue
-        # elif m < int(solns[i]) and 1 not in j:
-        #     continue
+        if s == int(solns[i]):
+            total += int(solns[i])
+            continue
+        elif s > int(solns[i]):
+            continue
+        m = product(j)
+        if m == int(solns[i]):
+            total += int(solns[i])
+            continue
+        elif m < int(solns[i]) and 1 not in j:
+            continue
 
-        listops = list(lister(['*', '+'], repeat=6))
+        listops = list(lister(['*', '+'], repeat=len(j)))
         listops = [list(k) for k in listops]
-        
-
-        # ops = ['+' for k in range(0, len(j)-1)]
-        # ops.append('')
         
         found = False
 
-        for k in range(len(j)):
-            # if ops[k] != '':
-            #     if ops[k] == '+':
-            #         ops[k] = '*'
-            #     else:
-            #         ops[k] = '+'
-            for l in range(len(j)):
-                # if ops[l] != '':
-                #     if ops[l] == '+' and ops[l] != '':
-                #         ops[l] = '*'
-                #     else:
-                #         ops[l] = '+'
-                ops = listops[l]
-                ops.append('')
-                print(ops)
+        for ops in listops:
+            ops.append('')
 
-                t = []
-                for m, n in enumerate(j):
-                    t.append(n)
-                    t.append(ops[m])
+            t = []
+            for m, n in enumerate(j):
+                t.append(n)
+                t.append(ops[m])
 
-                o = operate(t[0], t[1], t[2])
-                for m in range(3, len(t)-1, 2):
-                    o = operate(o, t[m], t[m+1])
+            o = operate(t[0], t[1], t[2])
+            for m in range(3, len(t)-1, 2):
+                o = operate(o, t[m], t[m+1])
 
-                print(ops, o)
-                
-                if o == int(solns[i]):
-                    total += o
-                    found = True
-                    break
-
-            if found:
+            if o == int(solns[i]):
+                total += o
+                found = True
                 break
-        
 
     return total
 
-print(pt1())
+def newoperate(a, op, b):
+    if op == '|':
+        r = int((str(a)+str(b)))
+    else:
+        r = eval(f'{a}{op}{b}')
+    return r
+
+def pt2():
+
+    total = 0
+
+    for i, j in enumerate(eqns):
+        j = [int(k) for k in j]
+
+        listops = list(lister(['*', '+', '|'], repeat=len(j)))
+        listops = [list(k) for k in listops]
+        
+        found = False
+
+        for ops in listops:
+            ops.append('')
+
+            t = []
+            for m, n in enumerate(j):
+                t.append(n)
+                t.append(ops[m])
+
+            o = newoperate(t[0], t[1], t[2])
+            for m in range(3, len(t)-1, 2):
+                o = newoperate(o, t[m], t[m+1])
+
+            if o == int(solns[i]):
+                total += o
+                found = True
+                break
+
+    return total
+
+print(pt2())
