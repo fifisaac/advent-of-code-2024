5ee48a70e7bfdb4edfa4f3a721a33d62b9184baf
fifisaac <99099211+fifisaac@users.noreply.github.com>
2024-12-08 19:04:22 +0000

    tidied day 8

--- day8/day8.py
+++ day8/day8.py
@@ -11,38 +11,28 @@ for i, j in enumerate(lines):
             else:
                 coorddict[lines[i][k]].append([i,k])
 
-def pt1():
+def findAntinodes():
 
-    antinodes = []
+    p1antinodes = []
+    p2antinodes = []
 
     for i in coorddict:
         for j in coorddict[i]:
             for l in coorddict[i]:
-                if (j[1] - l[1] == 0 or (j[0] - l[0]) / (j[1] - l[1])) and j != l:
+                if j != l:
                     loc = [j[0] + (j[0] - l[0]), j[1] + j[1] - l[1]]
-                    if loc not in antinodes and loc[0] in range(len(lines)) and loc[1] in range(len(lines[0])):
-                        antinodes.append(loc)
-
-    return(len(antinodes))
-
-print(f'Part 1: {pt1()}')
-
-def pt2():
-
-    antinodes = []
-
-    for i in coorddict:
-        for j in coorddict[i]:
-            for l in coorddict[i]:
-                if (j[1] - l[1] == 0 or (j[0] - l[0]) / (j[1] - l[1])) and j != l:
+                    if loc not in p1antinodes and loc[0] in range(len(lines)) and loc[1] in range(len(lines[0])):
+                        p1antinodes.append(loc)
                     k = 0
                     loc = [j[0] + k*(j[0] - l[0]), j[1] + k*(j[1] - l[1])]
                     while loc[0] in range(len(lines)) and loc[1] in range(len(lines[0])):
                         loc = [j[0] + k*(j[0] - l[0]), j[1] + k*(j[1] - l[1])]
-                        if loc not in antinodes and loc[0] in range(len(lines)) and loc[1] in range(len(lines[0])):
-                            antinodes.append(loc)
+                        if loc not in p2antinodes and loc[0] in range(len(lines)) and loc[1] in range(len(lines[0])):
+                            p2antinodes.append(loc)
                         k+=1
 
-    return(len(antinodes))
+    return(len(p1antinodes), len(p2antinodes))
 
-print(f'Part 2 {pt2()}')
+numantinode = findAntinodes()
+print(f'Part 1 {numantinode[0]}')
+print(f'Part 1 {numantinode[1]}')
